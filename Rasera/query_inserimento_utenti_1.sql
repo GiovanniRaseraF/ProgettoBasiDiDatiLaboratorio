57dfc52aa0bee4db76c5ee88201a260a07f81430
GiovanniRaseraF <fdsa@DESKTOP-KFD3S9A.localdomain>
2020-12-04 09:58:18 +0100

    Aggiornamento della query

--- Rasera/query_inserimento_utenti_1.sql
+++ Rasera/query_inserimento_utenti_1.sql
@@ -1,5 +1,4 @@
 INSERT INTO VALUES
-('NOME','COGNOME','CF','CITTA','INDIRIZZO'),
 ('Fidarma','Solombrino','SOIFIM33U20Q912N','Maniago','Via Tiziano vecellio 32'),
 ('Portos','Greco','GROPOT28U08E502Y','Cervignano del friuli','Piazza Guglielmo marconi 14'),
 ('Quinzia','Tunwal','TUWQUZ78N29U115A','Travesio','Via Forchiat 11'),
