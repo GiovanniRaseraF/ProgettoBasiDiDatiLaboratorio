e56740c8641800197c469ad90c7707822e4d3f64
GiovanniRaseraF <fdsa@DESKTOP-KFD3S9A.localdomain>
2020-12-04 15:36:00 +0100

    Aggiunta delle foto delle query DDL riscritte

--- Rasera/ddl.sql
+++ Rasera/ddl.sql
@@ -7,6 +7,52 @@ CREATE TABLE clienti(
     nome                        varchar(20)                 ,
     cognome                     varchar(20)                 ,
 
-    CONSTRAINT
-        cliente_pkey            PRIMARY KEY (codiceFiscalePartitaIva)
+    CONSTRAINT cliente_pkey            
+        PRIMARY KEY             (codiceFiscalePartitaIva)
 );
+
+
+
+CREATE TABLE capaceDiRisolvere(
+    codiceGuasto                integer             NOT NULL,
+    cfTecnico                   varchar(16)         NOT NULL,
+
+    CONSTRAINT capaceDiRisolvere_pkey  
+        PRIMARY KEY             (codiceGuasto, cfTecnico),
+    CONSTRAINT cfTecnico               
+        FOREIGN KEY             (cfTecnico)
+        REFERENCES              tecnici(codiceFiscale)
+        ON UPDATE               CASCADE
+        ON DELETE               CASCADE,
+    CONSTRAINT codiceGuasto            
+        FOREIGN KEY             (codiceGuasto)
+        REFERENCES              tipologiaGuasto(codiceGuasto)
+        ON UPDATE               CASCADE
+        ON DELETE               CASCADE
+);
+
+
+CREATE TABLE interventi(
+    numeroIntervento            integer             NOT NULL GENERATED ALWAYS AS IDENTITY(
+                                                        START       1
+                                                        MINVALUE    1
+                                                        MAXVALUE    2147483647
+                                                        INCREMENT   1
+                                                        CACHE       1),
+    data                        timestamp           NOT NULL,
+    durata                      integer             NOT NULL,
+    modalita                    character(1)        NOT NULL,
+    codiceRichiesta             integer             NOT NULL,
+    cfTecnico                   varchar(16)         NOT NULL,
+
+    CONSTRAINT interventi_pkey         
+        PRIMARY KEY             (codiceRichiesta, numeroIntervento),
+    CONSTRAINT cfTecnico               
+        FOREIGN KEY             (cfTecnico)
+        REFERENCES              tecnici(codiceFiscale)
+        ON UPDATE               CASCADE
+        ON DELETE               CASCADE
+);
+
+
+
